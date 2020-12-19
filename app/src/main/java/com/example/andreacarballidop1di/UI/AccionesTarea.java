228f5444132923901cdba904b424e17f4eca7f44
Andrea Carballido <andrea.carballido.ballesteros@gmail.com>
2020-12-19 11:43:06 +0100

    recuperacion

---
+++ app/src/main/java/com/example/andreacarballidop1di/UI/AccionesTarea.java
@@ -0,0 +1,10 @@
+package com.example.andreacarballidop1pmdm.UI;
+
+
+import com.example.andreacarballidop1pmdm.core.Entrenamiento;
+
+public interface AccionesEntrenamiento {
+    void mostrar(Entrenamiento entrenamiento);
+    void eliminar(Entrenamiento entrenamiento);
+    void modificar(Entrenamiento entrenamiento);
+}
